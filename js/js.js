f34da90370fe02836a08b02650ac87cd4a50ea3e
reem-abdelrazek20 <156788337+reem-abdelrazek20@users.noreply.github.com>
2025-08-03 23:17:11 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,3 +4,22 @@ const menu = document.getElementById('mobile-menu');
 btn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+
+  const images = [
+    "/img/home.jpg",
+    "/img/home2.jpg",
+    "/img/home3.jpg",
+   
+  ];
+
+  let currentIndex = 0;
+  const imageElement = document.getElementById("car");
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    imageElement.src = images[currentIndex];
+  }, 5000);
+
+
+
